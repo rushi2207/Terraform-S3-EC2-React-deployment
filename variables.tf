7640010f9c03a60bb43b38c1af9a35226a8e8eb1
Rushikesh Madhukar Sutar <155623277+rushi2207@users.noreply.github.com>
2025-09-29 23:04:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,23 @@
 variable "aws_region" {
-  default = "us-east-1"
+type = string
+default = "us-east-1"
 }
+
+
+variable "instance_type" {
+type = string
+default = "t2.micro"
+}
+
 
 variable "key_name" {
-  default     = "case-1"
-  description = "EC2 key pair name"
+type = string
+description = "Existing EC2 key pair name in the selected AWS account/region. If you don't have one create it or change this variable."
+default = "case-1"
 }
 
 
-variable "github_username" {
-  default     = "rushi2207"
-  description = "GitHub username"
-}
-
-
-
-variable "github_repo" {
-  default     = "https://github.com/rushi2207/Simple-react-app.git"
-  description = "GitHub repo URL"
+variable "bucket_name_prefix" {
+type = string
+default = "react-frontend"
 }
