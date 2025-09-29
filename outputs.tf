c4e1d14ff91783207a1a2c249d055767c95cb9e9
Rushikesh Madhukar Sutar <155623277+rushi2207@users.noreply.github.com>
2025-09-29 23:04:56 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,28 @@
-output "public_dns" {
-  value = aws_instance.react_app.public_dns
+output "instance_id" {
+value = aws_instance.react_app.id
+}
+
+
+output "instance_public_ip" {
+value = aws_eip.react_eip.public_ip
+}
+
+
+output "instance_public_dns" {
+value = aws_instance.react_app.public_dns
+}
+
+
+output "website_url" {
+value = format("http://%s", aws_eip.react_eip.public_ip)
+}
+
+
+output "s3_bucket_name" {
+value = aws_s3_bucket.frontend_bucket.bucket
+}
+
+
+output "s3_object_key" {
+value = aws_s3_bucket_object.frontend_zip.key
 }
